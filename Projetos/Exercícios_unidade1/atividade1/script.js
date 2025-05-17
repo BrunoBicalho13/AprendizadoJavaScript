d24b21f9b8137cdac0cc6cb608942ba38b89e874
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-05-17 07:38:31 -0300

    atividades 1 e 2

---
+++ Projetos/Exercícios_unidade1/atividade1/script.js
@@ -0,0 +1,5 @@
+// Escreva três valores em string em um arquivo e exiba no navegador com o condole.log
+
+console.log('Olá Mundo');
+console.log("Olá Mundo");
+console.log(`Olá Mundo`);
---
+++ Projetos/Exercícios_unidade1/atividade2/script.js
@@ -0,0 +1,5 @@
+// escreva três valores em number em um arquivo e exiba no navegador
+
+console.log(25);
+console.log(25.5);
+console.log((20 + 5) / 3 * 27);
