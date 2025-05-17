fbd70454b1ca25ab85472bcfb7aa67b0194b84eb
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-05-17 07:49:52 -0300

    Atividades 3 e 4

---
+++ Projetos/Exercícios_unidade1/atividade3/script.js
@@ -0,0 +1,5 @@
+// Escreva 3 comparações com boolean, uma com maior, menor ou igual e diferente
+
+console.log((24 + 3) > (25 +18));
+console.log((12 - 7) - (35 - 25));
+console.log((123 + 231) != (232 + 232));
---
+++ Projetos/Exercícios_unidade1/atividade4/script.js
@@ -0,0 +1,5 @@
+// Escreva três comparações com operadores lógicos;
+
+console.log((23 + 2) < 18 && 'Bruno' == 'Rodrigo');
+console.log('Daniel' == (12) || (25 + 5) == (20 + 10));
+console.log(!('Daniel' == 'Gabriel'));
