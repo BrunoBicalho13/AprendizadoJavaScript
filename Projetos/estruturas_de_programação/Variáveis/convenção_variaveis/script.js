8fd286e2b3e520beb38fb2a3f160ff881c6f5bfd
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-06-06 08:23:09 -0300

    Convencoes Variaveis

---
+++ Projetos/estruturas_de_programação/Variáveis/convenção_variaveis/script.js
@@ -0,0 +1,9 @@
+// Não se começa com número, mas se pode terminar com número
+//Pode ter $ ou _, mas não outro caracter especial
+
+// let 3nome = 'teste';
+
+let nome3 = 'teste';
+let $nome = 'teste2';
+let _nome = 'teste3';
+// let @nome = 'teste4';
---
+++ Projetos/estruturas_de_programação/Variáveis/declarar_variaveis_02/script.js
@@ -0,0 +1,7 @@
+var nome = "Bruno";
+const ip = 127313;
+
+console.log(nome);
+console.log(ip);
+
+// Não se pode  mudar o valor de uma constante
