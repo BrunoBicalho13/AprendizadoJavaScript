46389220b620c7f6b5506026f3790488c9cb4f16
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-06-06 10:08:21 -0300

    Switch

---
+++ Projetos/estruturas_de_controle/switch/script.js
@@ -0,0 +1,13 @@
+let nome = "Bruno";
+
+switch(nome){
+    case "Bruno":
+        console.log("O nome é Bruno");
+        break;
+    case "João":
+        console.log("O nome é João");
+        break;
+    default:
+        console.log("O nome não foi encontrado");
+        break;        
+}
