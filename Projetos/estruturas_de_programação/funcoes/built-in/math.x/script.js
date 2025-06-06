3bbd1179e16549df358e6cc6779524a9ac3b10ba
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-06-06 08:50:42 -0300

    math.x

---
+++ Projetos/estruturas_de_programação/funcoes/built-in/math.x/script.js
@@ -0,0 +1,18 @@
+// Utilizada para calculos matemáticos
+// Muito utilizada
+
+let maior = Math.max(6,12,28,55,8);
+
+console.log(maior);
+
+let menor = Math.min(6,12,28,55,8);
+
+console.log(menor);
+
+let arredondar = Math.round(5.293893);
+
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(5.293893);
+
+console.log(arredondarParaCima);
