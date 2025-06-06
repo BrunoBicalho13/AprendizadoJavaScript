1790383298301832ce4f623957dec0f53fc9bf3e
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-06-06 09:30:52 -0300

    else if

---
+++ Projetos/estruturas_de_controle/else if/script.js
@@ -0,0 +1,9 @@
+let nome = "Bruno";
+
+if (nome != undefined){
+    console.log("Nome está definido");
+} else if ( nome == "Bruno" && nome.length > 3){
+    console.log("O nome é Bruno");
+} else {
+    console.log("Não é Bruno!");
+}
