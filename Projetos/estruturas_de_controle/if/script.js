54ce45160a39d4e05835a55a5c06b2eb403c512e
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-06-06 09:22:46 -0300

    if

---
+++ Projetos/estruturas_de_controle/if/script.js
@@ -0,0 +1,21 @@
+let idade = 19;
+
+if (idade == 19){
+    console.log("A idade é 19");
+}
+
+if (idade> 25){
+    console.log("A idade é 25");
+}
+
+let nome = "Bruno";
+
+if (nome == "Bruno" && idade > 10){
+    console.log("Liberado");
+}
+
+let passaporte = true;
+
+if(nome == "Bruno" && idade > 30 ||  passaporte == true){
+    console.log("liberado! 2");
+}
