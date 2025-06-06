f6ea05a9402db7202aa82d8aae3fc829b939a0ff
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-06-06 08:40:00 -0300

    funcao built-in: prompt

---
+++ Projetos/estruturas_de_programação/funcoes/built-in/prompt/script.js
@@ -0,0 +1,10 @@
+//recebe e guarda o valor de um iput do usuario
+//pouco utilizada
+
+let idade = prompt("Qual a sua idade?");
+
+console.log(idade);
+
+let nome = prompt("Qual o seu nome?");
+
+console.log(`O seu nome é ${nome}`);
