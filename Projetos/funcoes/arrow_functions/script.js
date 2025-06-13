77ac7c22a6bd2fb585247a3243d64cbc87411fef
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-06-13 10:40:39 -0300

    mais sobre arrow functions

--- Projetos/funcoes/arrow_functions/script.js
+++ Projetos/funcoes/arrow_functions/script.js
@@ -9,3 +9,15 @@ let soma = (a,b) => {
 }
 
 console.log(soma(10,20));
+
+//mais enxuta
+
+const Quadrado2 = (x) => {
+    return x * x;
+}
+
+console.log(Quadrado2(2));
+
+const Quadrado = n => n * n;
+
+console.log(Quadrado(2));
