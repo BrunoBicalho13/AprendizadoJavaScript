da37f88bbb384512cdbd5eb9f5bccc9ae69c4dc9
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-08-24 22:51:16 -0300

    Debugs

---
+++ Projetos/Debugs_e_tratamento_de_erros/Strict_mode/script.js
@@ -0,0 +1,18 @@
+//Deve ser declarado no topo de arquivos ou funções
+//Ajuda a codar de forma correta e não prejudica o Código
+
+"Use strict"
+
+let opa = "teste";
+
+//delete Object.prototype
+function teste(){
+    "Use strict"
+
+    let testando = 'teste';
+}
+
+teste();
+
+//false,prop = "";
+//"teste".prop = "";
