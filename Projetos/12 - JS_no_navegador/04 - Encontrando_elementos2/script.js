a0610c436818a30421902cb9e5f333a9f4a20cff
Bruno_Bicalho <brunobicalho6@gmail.com>
2025-08-26 21:14:52 -0300

    JS na web + DOM

---
+++ Projetos/12 - JS_no_navegador/04 - Encontrando_elementos2/script.js
@@ -0,0 +1,29 @@
+//Pelo tag name
+
+console.log(document.getElementsByTagName('h1'));
+
+console.log(document.getElementsByTagName('p'));
+
+console.log(document.getElementsByTagName('li'));
+
+//Por Id
+
+console.log(document.getElementById('titulo-principal'));
+
+//Pela classe
+
+console.log(document.getElementsByClassName('itens-azuis'));
+
+console.log(document.getElementsByClassName('itens-vermelhos'));
+
+//Por query
+
+console.log(document.querySelector('#container-principal h1'));
+
+console.log(document.querySelector('#segundo-container h1'));
+
+console.log(document.querySelector('div div p'));
+
+console.log(document.querySelector('footer h2'));
+
+console.log(document.querySelector('ul .itens-azuis'));
---
+++ Projetos/12 - JS_no_navegador/13 -  alterando_css/script.js
@@ -0,0 +1,13 @@
+let elemento = document.querySelector('#titulo-principal');
+
+setTimeout(function() {
+    
+    elemento.style.color ='red';
+     
+    elemento.style.backgroundColor ='pink';
+
+    elemento.style.width ='400px';
+
+},1000)
+
+
